782b4975f5532af4654fc02eaff3d77c2cb655fd
Zongo Maqutu <MQTZON001@myuct.ac.za>
2021-04-25 23:09:34 +0200

    Added copy and move constructors to ConnectedComponent

--- src/ConnectedComponent.cpp
+++ src/ConnectedComponent.cpp
@@ -7,18 +7,35 @@
 #include "ConnectedComponent.h"
 
 namespace MQTZON001 {
-	ConnectedComponent::ConnectedComponent(int id){
-		pixelCount = 0;
-		setComponentId(id);
+	ConnectedComponent::ConnectedComponent(int id)
+		:componentId(id),pixelCount(0)
+	{
 	}
+	//destructor
 	ConnectedComponent::~ConnectedComponent(){
+		pixelIndexes.clear();
+		setPixelCount(0);
+		setComponentId(0);
 		std::cout << "Component Destroyed " << std::endl;
+	}
+	//copy constructor
+	ConnectedComponent::ConnectedComponent(const ConnectedComponent & rhs)
+		:componentId(rhs.componentId),pixelCount(rhs.pixelCount),pixelIndexes(rhs.pixelIndexes)
+	{
+	}
+	//move constructor move(int) just copies anyway so no need to it
+	ConnectedComponent::ConnectedComponent(ConnectedComponent && rhs)
+		:componentId(rhs.componentId),pixelCount(rhs.pixelCount),pixelIndexes(std::move(rhs.pixelIndexes))
+	{
 	}
 	int ConnectedComponent::getId(){
 		return componentId;
 	}
 	int ConnectedComponent::getPixelCount(void) const {
 		return pixelCount;
+	}
+	void ConnectedComponent::setPixelCount(int count) const{
+		pixelCount = count;
 	}
 	void ConnectedComponent::setComponentId(int containerId){
 		componentId = containerId;
--- src/ConnectedComponent.h
+++ src/ConnectedComponent.h
@@ -1,18 +1,24 @@
 #include <string>
+#include <unordered_set>
+#include <boost/functional/hash.hpp>
 #ifndef ConnectedComponent_H
 #define ConnectedComponent_H
 
 namespace MQTZON001{
 	class ConnectedComponent {
-		private:
-                        int pixelCount;
+		private: 
                         int componentId;
-			//a way to store pixels in this component
+			int pixelCount;
+			//a way to store pixels in this component an unordered set or pairs
+			std::unordered_set<std::pair<int,int>,boost::hash<std::pair<int, int>>> pixelIndexes;
                 public:
 			ConnectedComponent(int id);
 			~ConnectedComponent();
+			ConnectedComponent(const ConnectedComponent & rhs);
+			ConnectedComponent(ConnectedComponent && rhs);
 			int getComponentId();
 			int getPixelCount(void) const;
+			void setPixelCount(void) const;
 			void setComponentId(int no);
 	};
 }
