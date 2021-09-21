37ce280c94a955c0d8c337e152128be38092b1eb
Zongo Maqutu <MQTZON001@myuct.ac.za>
2021-09-21 22:09:25 +0200

    successfully implemented python script for editing viewports

--- NFT Generation/nft_viewport_editor.py
+++ NFT Generation/nft_viewport_editor.py
@@ -2,10 +2,18 @@ import os
 import sys
 import fileinput
 
-with open('nft_161.svg','r') as file:
-    filedata = file.read();
 
-filedata = filedata.replace('width="3721.000000pt" height="2392.000000pt"','width="100%" height="100%"')
 
-with open('nft_161.svg','w') as file:
-    file.write(filedata)
+for edition in range(256):
+    
+    file_name = '../output_files/nfts/Home With Moon/Final NFT Collection/White First SVG/'+ 'nft_' + str(edition) + '.svg'
+
+    with open(file_name,'r') as file:
+        filedata = file.read()
+    
+    filedata = filedata.replace('width="3721.000000pt" height="2392.000000pt"','width="100%" height="100%"')
+    
+
+    with open(file_name,'w') as file:
+        file.write(filedata)
+        print(file_name + ' edit complete...')
